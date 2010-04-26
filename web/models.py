338c7f461e756ff9e0ed86d0101cd5603c73c3e2
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-04-26 14:30:10 -0700

    more work on login and registration pages

--- web/models.py
+++ web/models.py
@@ -20,3 +20,8 @@ class LoginForm(forms.Form):
 	username = forms.CharField(max_length=255)
 	password = forms.CharField(max_length=255)
 
+class RegisterForm(forms.Form):
+	username = forms.CharField(max_length=255)
+	email = forms.CharField(max_length=255)
+	password = forms.CharField(max_length=255)
+
--- web/views.py
+++ web/views.py
@@ -1,16 +1,27 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-from web.models import Event, UploadForm, LoginForm
+from web.models import Event, UploadForm, LoginForm, RegisterForm
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 def index(request):
     return render_to_response('index.html', {})
 
 def register(request):
-    return render_to_response('register.html', {})
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('./')
+		else:
+			return HttpResponse('Form submission failed')
+	else:
+		return render_to_response('register.html', {})
 
 def raids(request):
     return render_to_response('raids.html', {})
+
+def raid_detail(request):
+	return render_to_response('raid_detail.html', {})
 
 def upload(request):
 	if request.method == 'POST':
