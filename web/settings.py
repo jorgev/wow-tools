3c84380ed4cd855e6f31e0ef9a678e1f9a85ee2b
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-05-01 00:36:49 -0700

    added template path for mac os x, switching dev server to use default host and port

--- web/settings.py
+++ web/settings.py
@@ -69,7 +69,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/jorge/wow-tools/templates',
+	'/home/jorge/wow-tools/templates', # for linux
+	'/Users/jorge/wow-tools/templates', # for mac os x
 )
 
 INSTALLED_APPS = (
--- web/start_server.sh
+++ web/start_server.sh
@@ -1,2 +1,2 @@
-sudo python manage.py runserver 0.0.0.0:80
+python manage.py runserver
 
