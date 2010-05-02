c596434f69182c354a0a16e4b1cc13fe1e6c6d76
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-05-01 22:12:50 -0700

    implemented ajax restful delete call from raid listing page

--- media/js/raids.js
+++ media/js/raids.js
@@ -1,10 +1,20 @@
 // raids.js
 
+var rowToDelete;
+
 $(document).ready(function() {
 	// confirm user action
 	$('.delete').click(function() {
 		var name = $(this).parent().children('.name').text();
-		if (!confirm('Are you sure you want to remove ' + name + ' from your raid listings?'))
+		if (!confirm('Are you sure you want to delete ' + name + ' from your raid listings?'))
 			return false;
+		rowToDelete = $(this).parent().parent();
+		$.ajax({
+			type: 'DELETE',
+			url: $(this).parent().children('a').attr('href'),
+			success: function(data) {
+				rowToDelete.remove();
+			}
+		});
 	});
 });
