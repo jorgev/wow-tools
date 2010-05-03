c37d370152e9c653eaec5db14eb450d2d6bb8773
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-05-03 01:18:44 -0700

    remove auto-expansion of 4th child

--- media/js/raid_detail.js
+++ media/js/raid_detail.js
@@ -7,19 +7,12 @@ function createTreeFromCombatData(node, data) {
 	node.setHtml(data[0]);
 	if (data.length > 1) {
 		var children = data[1];
-		var childNotCollapsible = 3; // Hard coded to reduce randomness.
 		for (var i = 0; i < children.length; i++) {
 			var child = children[i];
 			var childNode = node.getTree().createNode();
 
 			node.add(childNode);
 			createTreeFromCombatData(childNode, child);
-
-			if (i == childNotCollapsible && child.length > 1) {
-				childNode.setIsUserCollapsible(false);
-				childNode.setExpanded(true);
-				nonCollapseNode = childNode;
-			}
 		}
 	}
 }
