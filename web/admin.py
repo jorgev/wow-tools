1a5f5519ee899aef06f1956ce4bd1c3db7cd9050
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-05-04 00:39:05 -0700

    customized the admin page display for the events

--- web/admin.py
+++ web/admin.py
@@ -2,7 +2,10 @@ from web.models import Event
 from django.contrib import admin
 
 class EventAdmin(admin.ModelAdmin):
-	fields = ['name', 'html']
+	fields = ['name', 'user', 'html']
+	list_display = ('name', 'user', 'created')
+	list_filter = ['user']
+	search_fields = ['user']
+	date_hierarchy = 'created'
 
 admin.site.register(Event, EventAdmin)
-
