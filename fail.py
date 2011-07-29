41ce825d9c1c9a6102ca0a1b3b8dfa4aae06bd87
Jorge Velázquez <jorge.velazquez@gmail.com>
2011-07-28 19:08:44 -0700

    using csv module for better parsing

--- fail.py
+++ fail.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import csv
 import sys
 
 NPC_EFFECTS = { 'Temple Guardian Anhuur': [],
@@ -14,23 +15,22 @@ def main(argv=None):
 		argv = sys.argv
 	filename = argv[1]
 
-	for line in (x.strip() for x in open(filename)):
-		major_split = line.split('  ')
-		timestamp = major_split[0]
-		fields = major_split[1].split(',')
+	reader = csv.reader(open(filename))
+	for row in reader:
+		timestamp, event = row[0].split('  ')
 		
 		# the data we are interested in all has 21 fields in it, so we can verify that up front
-		if len(fields) == 21:
+		if len(row) == 21:
 			# we're only interested in special effects, which fall into these categories
-			if fields[0] in ['SPELL_DAMAGE', 'SPELL_PERIODIC_DAMAGE']:
-				source = fields[2][1:-1]
+			if event in ['SPELL_DAMAGE', 'SPELL_PERIODIC_DAMAGE']:
+				source = row[2]
 				if source in NPC_EFFECTS:
-					effect = fields[10][1:-1]
+					effect = row[10]
 					if effect in NPC_EFFECTS[source]:
 						# we're only interested in players
-						target_flags = int(fields[7], 16)
+						target_flags = int(row[7], 16)
 						if target_flags & 0x400:
-							print fields[6][1:-1], 'hit by', effect, 'for', fields[12]
+							print row[6], 'hit by', effect, 'for', row[12]
 
 if __name__ == "__main__":
 	sys.exit(main())
